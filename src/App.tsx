0bb8cb132da98a35594f7755b50f7f52b0e3001d
Grunkans <143597576+Grunkans@users.noreply.github.com>
2024-10-29 14:42:39 +0100

    lagt in mall på frontenden

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,65 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
-  return (
-    <p>"Hello fullstack"</p>
+	return (
+	  <div>
+	  <header>
+	  <h1> Chappy </h1>
+	<div className="user-status">
+		<span>Inloggad som VänligaVera</span>
+		<button> Logga ut </button>
+	</div>
+	</header>
+	<main>
+	<nav>
+		<ul>
+			<li> [Kanaler] </li>
+			<li><a href="#"> #koda </a></li>
+			<li><a href="#"> #random </a> <span className="unread">3</span> </li>
+			<li className="locked"><a href="#"> #grupp1 🔒 </a></li>
+			<li className="selected"><a href="#"> #grupp2 🔑 </a></li>
+			<li className="locked"><a href="#"> #grupp3 🔒 </a></li>
+			<li> <hr/> </li>
+			<li title="Direktmeddelanden"> [DM] </li>
+			<li><a href="#">PratgladPelle</a></li>
+			<li><a href="#">SocialaSara</a></li>
+			<li><a href="#">TrevligaTommy</a></li>
+			<li><a href="#">VänligaVera</a></li>
+			<li><a href="#">GladaGustav</a></li>
+		</ul>
+	</nav>
+	<div className="chat-area">
+		<section className="heading">
+			Chattar i <span className="chat-name"> #grupp2 </span>
+		</section>
+	<section className="history">
+		
+		<section className="align-right">
+			<p> VänligaVera: hejsan </p>
+			<p> 17:46 </p>
+		</section>
+		
+		<section>
+			<p> MunterMoa: tjena! </p>
+			<p> 17:47 </p>
+		</section>
+		
+		<section>
+			<p> GladaGustav: hallå! </p>
+			<p> 17:48 </p>
+		</section>
+		
+	</section>
+	<section>
+		<input type="text" placeholder="Ditt meddelande..." />
+		<button> Skicka </button>
+	</section>
+	</div>	
+	</main>
+	  </div>
+	
   )
 }
 
